4638545b5277383794f46fa9e4ca2bc08be441d1
El-Bambu <remember884@gmail.com>
2025-03-01 18:51:49 +0100

    se

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <h1>Uatashiwa</h1>
         <Image
           src="/assets/L29.webp"
           alt="Next.js logo"
@@ -18,7 +19,7 @@ export default function Home() {
           height={58}
           priority
         />
-        <h1>THE CEO HIMSELF</h1>
+        <h1>The CEO/CTO Himself!</h1>
       </main>
     </div>
   );
